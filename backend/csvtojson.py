54d76e37cda984651076797a2498fa07af3ac192
kawtarknd <kawtar.kinadk17@example.com>
2025-05-19 00:43:08 +0100

    Backend data preparation

---
+++ backend/csvtojson.py
@@ -0,0 +1,22 @@
+import csv
+import json
+
+# Ouvre le fichier CSV
+with open('C:/Users/lenovo/Desktop/M1/M1 S2/systRetD/Projet/moteur-recherche-distribue/logstash/data/ai_projects.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    json_data = []
+
+    for row in reader:
+        json_entry = {
+            "title": row["Project"],
+            "source": row["Insitution / Company"] or "Unknown",
+            "date": row["Year"] or "Unknown",
+            "message": row["Description"],
+            "tags": [row["Type"], row["Category"]],
+            "url": row["Homepage or Source Code"]
+        }
+        json_data.append(json_entry)
+
+with open('projets2_ai.json', 'w', encoding='utf-8') as jsonfile:
+    for entry in json_data:
+        jsonfile.write(json.dumps(entry, ensure_ascii=False) + '\n')
